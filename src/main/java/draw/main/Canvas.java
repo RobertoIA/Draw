89e9b9bb42bca88424fb60fa9a196913c1974920
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-02-20 23:09:27 +0100

    Added basic drawing capabilities.

--- src/main/java/draw/main/Canvas.java
+++ src/main/java/draw/main/Canvas.java
@@ -3,7 +3,15 @@ package draw.main;
 import processing.core.PApplet;
 
 @SuppressWarnings("serial")
-public class Canvas extends PApplet{
+public class Canvas extends PApplet {
+	public void setup() {
+		background(255);
+	}
 
-
+	public void draw() {
+		if (mousePressed) {
+			fill(255, 0, 0);
+			line(pmouseX, pmouseY, mouseX, mouseY);
+		}
+	}
 }
