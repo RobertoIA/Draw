4c7c0c0b683dffbaab706e0e766181dd4b5656fd
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-02-26 15:55:59 +0100

    Support for bursh styles and new brush.

--- src/main/java/draw/main/Canvas.java
+++ src/main/java/draw/main/Canvas.java
@@ -1,14 +1,21 @@
 package draw.main;
 
 import java.awt.Color;
+import java.util.Random;
 
 import processing.core.PApplet;
 
 @SuppressWarnings("serial")
 public class Canvas extends PApplet {
 
+	private static enum Brush {
+		NORMAL, FUZZY
+	}
+
 	private Color color = new Color(0, 0, 0);
 	private int width = 1;
+	private Brush brush = Brush.FUZZY;
+	private Random random = new Random();
 
 	public void setup() {
 		background(255);
@@ -16,9 +23,14 @@ public class Canvas extends PApplet {
 
 	public void draw() {
 		if (mousePressed) {
-			stroke(color.getRed(), color.getGreen(), color.getBlue());
-			strokeWeight(width);
-			line(pmouseX, pmouseY, mouseX, mouseY);
+			switch (brush) {
+			case NORMAL:
+				normalBrush();
+				break;
+			case FUZZY:
+				fuzzyBrush();
+				break;
+			}
 		}
 	}
 
@@ -32,5 +44,27 @@ public class Canvas extends PApplet {
 
 	public void reset() {
 		background(255);
+	}
+
+	public void setNormalBrush() {
+		this.brush = Brush.NORMAL;
+	}
+
+	private void normalBrush() {
+		stroke(color.getRed(), color.getGreen(), color.getBlue());
+		strokeWeight(width);
+		line(pmouseX, pmouseY, mouseX, mouseY);
+	}
+
+	public void setFuzzyBrush() {
+		this.brush = Brush.FUZZY;
+	}
+
+	private void fuzzyBrush() {
+		int offset = (int) ((random.nextFloat() / 2) * width);
+		stroke(color.getRed(), color.getGreen(), color.getBlue());
+		strokeWeight(width);
+		line(pmouseX - offset, pmouseY - offset, mouseX + offset, mouseY
+				+ offset);
 	}
 }
